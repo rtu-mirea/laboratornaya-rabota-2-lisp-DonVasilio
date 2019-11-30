b9bce33c0d932459ee4e4ca474aeba871869eb55
DonVasilio <47894225+DonVasilio@users.noreply.github.com>
2019-11-30 11:33:49 +0300

    Update and rename 2 to 2.lisp

--- 2.lisp
+++ 2.lisp
@@ -1,5 +1,5 @@
 (defun readfile(file)
-(let ((in (open file :direction :input)))
+(let ((in (open file :direction :input :if-does-not-exist nil)))
     (when in
     (loop for line = (read-line in nil)
          while line do (format t "~a~%" line))
